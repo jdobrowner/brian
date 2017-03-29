fc51b50f6c5c893d7ba5447917b9e9ba41d272d4
Jason Dobrowner <jdobrowner@gmail.com>
2017-03-29 15:58:21 -0700

    Update contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,7 +5,7 @@ function Contact() {
       <div className="contact">
         <h3>| contact |</h3>
         <p>Email or call me to discuss your project.</p><br />
-        <a className="email-me" href="mailto:brian4bes@gmail.com">brian4bes@gmail.com</a>
+        <a className="email-me" href="mailto:brianforbescreative@gmail.com">brianforbescreative@gmail.com</a>
         <h5>818 438 0216</h5>
       </div>
     )
